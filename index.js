e000e785038ac817ead682caef5dea688620ec2d
yasashiku <supamu.no.akaunto@gmail.com>
2017-11-14 10:21:08 -0700

    Fix import issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Toolbar, AppToolbar } from './Toolbar/index.js';
 import { NewFileDialog } from './Dialog/index.js';
 import DocView from './DocView/index.js';
 import TreeView from './TreeView/index.js';
-import updateImgColorFilters from './imgColorFilters/index.js';
+import updateImgColorFilters from './ImgColorFilters/index.js';
 import applySvgButtonColors from './svgButtons/index.js';
 
 export { Toolbar, AppToolbar };
